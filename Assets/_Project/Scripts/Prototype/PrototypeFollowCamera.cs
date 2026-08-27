95dc5a65f7c1a422bc0ca27d835b21fc22299b53
ShuFukushima <keiranhiragi@gmail.com>
2026-08-27 14:47:01 +0900

    feat(camera): 基本的な車両追従カメラを追加

---
+++ Assets/_Project/Scripts/Prototype/PrototypeFollowCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PrototypeFollowCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _followObj; // カメラが追う対象
+    [SerializeField] private Vector3 _offsetPos;    // カメラ座標の調整
+
+    private void LateUpdate()
+    {
+        Vector3 followPosition = _followObj.transform.TransformPoint(_offsetPos);
+
+        // 動作確認として座標を合わせる
+        gameObject.transform.position = followPosition;
+
+        // 回転を合わせる
+        // 対象オブジェクトに対して注目させる という処理に変更する
+        gameObject.transform.LookAt(_followObj.transform);
+    }
+}
