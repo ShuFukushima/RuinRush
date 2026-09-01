0c612feed809f4960566f58b16de507020580de2
ShuFukushima <keiranhiragi@gmail.com>
2026-09-01 17:46:32 +0900

    refactor(prefab): 車両用のモデルを適用　廃墟モデルの本モデルを適用　廃墟Prefabの構成を整理

--- Assets/_Project/Scripts/Prototype/PrototypeTireVisualController.cs
+++ Assets/_Project/Scripts/Prototype/PrototypeTireVisualController.cs
@@ -5,7 +5,10 @@ public class PrototypeTireVisualController : MonoBehaviour
     Vector3 _pos;        //タイヤの座標を管理する変数
     Quaternion _rot;     //タイヤの回転を管理する変数
     [SerializeField] WheelCollider _wc;   //それぞれのWheelCollider
+    [SerializeField] private bool _isRight = false; // 左右で切り替え
     Transform _wheel;    //それぞれのタイヤ
+
+
 
     void Start()
     {
@@ -16,6 +19,16 @@ public class PrototypeTireVisualController : MonoBehaviour
     {
         _wc.GetWorldPose(out _pos, out _rot);  //WheelColliderから位置と回転情報を取得
         _wheel.transform.position = _pos;     //タイヤの位置を指定
-        _wheel.transform.rotation = _rot;     //タイヤの角度を指定
+
+        // タイヤが右についているか左についているかで分ける
+        if (_isRight)
+        {
+            _wheel.transform.rotation = _rot * Quaternion.Euler(0f, 180f, 0f);
+        }
+        else
+        {
+            _wheel.transform.rotation = _rot;     //タイヤの角度を指定
+        }
+
     }
 }
